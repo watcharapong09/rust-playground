b940ea5c1a50dccd4b65ff3546b5dae7abf5c262
ake <noo-ake@hotmail.com>
2024-03-31 00:54:24 +0700

    update

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,45 @@
-use std::{env, process};
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    thread,
+};
 
-use rust_playground::Config;
+struct ThreadPool;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
-        process::exit(1);
-    });
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
-    if let Err(e) = rust_playground::run(config) {
-        println!("Application error: {e}");
-        process::exit(1);
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        ThreadPool
     }
 }
 
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        thread::spawn(|| {
+            handle_connection(stream);
+        });
+    }    
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
