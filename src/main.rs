bed1342d631a2878dba8fd8e6ba10fd6a0745491
ake <noo-ake@hotmail.com>
2024-03-31 12:17:54 +0700

    update

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,7 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
-    thread,
 };
-
-struct ThreadPool;
-
-impl ThreadPool {
-    pub fn new(size: usize) -> ThreadPool {
-        ThreadPool
-    }
-}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -19,16 +10,18 @@ fn main() {
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        thread::spawn(|| {
-            handle_connection(stream);
-        });
+        handle_connection(stream);
     }    
 }
 
 fn handle_connection(mut stream: TcpStream) {
+    println!("A");
     let buf_reader = BufReader::new(&mut stream);
+
+    println!("{:#?}", buf_reader);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
+    println!("B");
     let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
         ("HTTP/1.1 200 OK", "hello.html")
     } else {
@@ -36,10 +29,13 @@ fn handle_connection(mut stream: TcpStream) {
     };
 
     let contents = fs::read_to_string(filename).unwrap();
+
+    println!("C");
     let length = contents.len();
 
-    let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
+
+    println!("D");
 }
